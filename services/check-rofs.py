195ed1efd9d52da54d08b6bbb316a4a41b5f88a0
Alan Christie <alan.christie@matildapeak.com>
2024-10-17 09:18:28 +0200

    feat: Major change for ROFS checker

---
+++ services/check-rofs.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# Run with: -
+#  python check-no-rofs.py [period_minutes]
+
+import os
+import subprocess
+import sys
+import time
+
+# The command(s) to run the playbook...
+_DEV_CMD = [
+    "ansible-playbook",
+    "site-check-rofs.yaml",
+    "--vault-password-file",
+    "vault-pass-services.txt",
+    "--extra-vars",
+    "cluster=dev",
+]
+
+_PROD_CMD = [
+    "ansible-playbook",
+    "site-check-rofs.yaml",
+    "--vault-password-file",
+    "vault-pass-services.txt",
+    "--extra-vars",
+    "cluster=prod",
+]
+
+# Default period between playbook runs...
+# (which can be changed by providing a numeric (greater than 10) command line argument)
+_PERIOD_MINUTES = 10
+if len(sys.argv) > 1:
+    _PERIOD_MINUTES = int(sys.argv[1])
+_PERIOD_MINUTES = max(_PERIOD_MINUTES, 10)
+
+# For this playbook we need a vault password file...
+if not os.path.exists("vault-pass-services.txt"):
+    print("You must provide a vault-pass-services.txt file")
+    sys.exit(1)
+
+# Go - continuously...
+while True:
+    print("+> Running the DEV playbook...")
+    result = subprocess.run(_DEV_CMD, check=True)
+    if result.returncode != 0:
+        sys.exit(2)
+    print("+> Running the PROD playbook...")
+    result = subprocess.run(_PROD_CMD, check=True)
+    if result.returncode != 0:
+        sys.exit(2)
+    print(f"+> Sleeping ({_PERIOD_MINUTES})...")
+    time.sleep(_PERIOD_MINUTES * 60)
